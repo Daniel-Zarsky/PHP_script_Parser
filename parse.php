02e48be14e439d68be7e2a4598c17516b75d40ac
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2023-02-20 14:04:18 +0100

    Argument checking done

--- parse.php
+++ parse.php
@@ -1,4 +1,27 @@
 <?php
-$txt = "PHP";
-echo "I love $txt!";
+ 
+ ini_set('display_errors', 'stderr');
+
+        // arg check
+        if ($argc > 1) {
+
+            if ($argv[1] == "--help" && $argc == 2) { //help
+                echo("help");
+                exit(0);
+            }
+            else if(substr_compare($argv[1], "--source", 0, 8) == 0){ // source file
+                $source_file = substr($argv[1], 9, strlen($argv[1]));
+            }
+            else exit(10); // error
+        }
+
+        // loading input
+        while($line = fgets(STDIN)){
+            echo($line);
+        }
+
+
+
+    
+
 ?>
