7f1874c600cc41a3283553c7cf7ce1818bc9d571
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2023-03-04 11:53:17 +0100

    3 failed extended tests

--- parse.php
+++ parse.php
@@ -1,36 +1,34 @@
 <?php
-
-       include 'parse_lib.php';
-
+        /**
+        * Project: IPP 2023 Parser
+        * @file parse.php
+        * @brief Script for syntactic and lexical analysis of IPPcode23.
+        * @author Daniel Žárský <xzarsk04@stud.fit.vutbr.cz>
+        */
+        include 'parse_lib.php';
        ini_set('display_errors', 'stderr');
 
        echo("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 
-        // arg check
+        // argument  check
         if ($argc > 1) {
 
             if ($argv[1] == "--help" && $argc == 2) { //help
-                echo("help");
+                echo("Usage php parse.php < input_file \n");
                 exit(0);
-            }
-            else if(substr_compare($argv[1], "--source", 0, 8) == 0){ // source file
-                $source_file = substr($argv[1], 9, strlen($argv[1]));
             }
             else exit(10); // bad argument
         }
 
         $header = false;
-        $in_count =0;
-        global $output;
+        $in_count =0;  //instruction count
 
         while($line = fgets(STDIN)){
-
-
 
             $line = cut_comment($line); //cut comment
 
               if(!$header){             //check header
-                  if(trim($line) == ".IPPcode23"){
+                  if(strtoupper(trim($line)) == strtoupper(".IPPcode23")){
                       $header =true; //header ok
                       echo("<program language=\"IPPcode23\">\n");
                       continue;
@@ -43,21 +41,24 @@
                   }
               }
 
-            // $lexemes = explode(" ", trim($line, '\n')); //split the input into lexemes, trim empty lines
-
-            $lexemes = preg_split('/\s+/', $line);
+            $lexemes = preg_split('/\s+/', $line); ///separating into lexemes
 
              if($lexemes[0]=="" || str_contains($lexemes[0], "#")){ //empty array after cutting new line
                  continue;
              }
 
              $in_count ++; //new valid instruction
-             $inst = $lexemes[0]; ///after cutting and trimming the first wor in line is instruction
+             $inst = $lexemes[0]; ///after cutting and trimming the first word in line is instruction
 
-             switch(trim(strtoupper($inst))){
+             switch(trim(strtoupper($inst))){   ///sorting the input based on first lexeme
 
+                 ///<INSTRUCTION> <VARIABLE>
                  case'DEFVAR':
                  case 'POPS':
+                     if(count($lexemes)!=2){
+                         exit(23);
+                     }
+
                      echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
                      if(check_variable(trim($lexemes[1]))){
                          echo("  <arg1 type=\"var\">".convert_string($lexemes[1])."</arg1>\n");
@@ -68,17 +69,19 @@
 
                      echo(" </instruction>\n");
 
-                     if(count($lexemes)>2){
-                         exit(23);
-                     }
+
                      break;
 
+                 ///<INSTRUCTION> <VARIABLE> <SYMBOL>
                  case'MOVE':
-
                  case 'INT2CHAR':
                  case 'STRLEN':
                  case 'TYPE':
                  case 'NOT':
+
+                     if(count($lexemes)!=3){
+                         exit(23);
+                     }
 
                      echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
                      if(check_variable($lexemes[1])){
@@ -88,38 +91,33 @@
                          exit(23);
                      }
 
-
                      if(check_symbol($lexemes[2])){
                          echo("  <arg2 type=\"".substr($lexemes[2], 0, strpos($lexemes[2], "@"))."\">".convert_string(substr($lexemes[2], strpos($lexemes[2], "@")+1,strlen($lexemes[2])-strpos($lexemes[2], "@")))."</arg2>\n");
 
                      }
                      else if(check_variable($lexemes[2])){
                          echo("  <arg2 type=\"var\">".$lexemes[2]."</arg2>\n");
-
                      }
                      else{
-
-                         exit(23);
-
-                     }
-
-                     if(count($lexemes)>3){
                          exit(23);
                      }
 
                      echo(" </instruction>\n");
                      break;
 
+                 ///<INSTRUCTION>
                  case'PUSHFRAME':
                  case'CREATEFRAME':
                  case'POPFRAME':
                  case 'BREAK':
-                     echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\"/>\n");
-                     if(count($lexemes)>1){
+                     if(count($lexemes)!=1){
                          exit(23);
                      }
+                     echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\"/>\n");
+
                      break;
 
+                 ///<INSTRUCTION> <LABEL>
                  case 'LABEL':
                  case 'JUMP':
                  case'CALL':
@@ -138,14 +136,16 @@
                      }
                      break;
 
+                 ///<INSTRUCTION>
                  case'RETURN':
-                     echo(" <instruction order=\"".$in_count."\" opcode=\"RETURN\"/>\n");
-                     if(count($lexemes)>1){
+                     if(count($lexemes)!=1){
                          exit(23);
                      }
+                     echo(" <instruction order=\"".$in_count."\" opcode=\"RETURN\"/>\n");
+
                      break;
 
-
+                 ///<INSTRUCTION> <VARIABLE> <SYMBOL> <SYMBOL>
                  case 'ADD':
                  case 'SUB':
                  case 'MUL':
@@ -163,7 +163,9 @@
                  case 'GETCHAR':
                  case 'CONCAT':
                  case 'SETCHAR':
-
+                     if(count($lexemes)!=4){
+                         exit(23);
+                     }
                      echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
                      if(check_variable(trim($lexemes[1]))){
                          echo("  <arg1 type=\"var\">".$lexemes[1]."</arg1>\n");
@@ -181,7 +183,6 @@
                          exit(23);
                      }
 
-
                      if (check_symbol($lexemes[3])) {
                          echo("  <arg3 type=\"".substr($lexemes[3], 0, strpos($lexemes[3], "@"))."\">".convert_string(substr($lexemes[3], strpos($lexemes[3], "@") + 1, strlen($lexemes[3]) - strpos($lexemes[3], "@")))."</arg3>\n");
                      } else if (check_variable($lexemes[3])) {
@@ -190,15 +191,14 @@
                          exit(23);
                      }
 
-
-                     if(count($lexemes)>4){
-                         exit(23);
-                     }
-
                      echo(" </instruction>\n");
                      break;
 
+                 ///<INSTRUCTION> <VARIABLE> <TYPE>
                  case 'READ':
+                     if(count($lexemes)!=3){
+                         exit(23);
+                     }
                      echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
                      if(check_variable($lexemes[1])){
                          echo("  <arg1 type=\"var\">".$lexemes[1]."</arg1>\n");
@@ -213,17 +213,18 @@
                      else{
                          exit(23);
                      }
-                     if(count($lexemes)>3){
-                         exit(23);
-                     }
 
                      echo(" </instruction>\n");
                      break;
 
+                 ///<INSTRUCTION> <VARIABLE>
                  case 'WRITE':
                  case 'EXIT':
                  case 'DPRINT':
                  case 'PUSHS':
+                     if(count($lexemes)!=2){
+                         exit(23);
+                     }
                      echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
                      if(check_symbol($lexemes[1])){
                          echo("  <arg1 type=\"".substr($lexemes[1], 0, strpos($lexemes[1], "@"))."\">".convert_string(substr($lexemes[1], strpos($lexemes[1], "@")+1, strlen($lexemes[1])-strpos($lexemes[1], "@")))."</arg1>\n");
@@ -236,15 +237,15 @@
                      }
                      echo(" </instruction>\n");
 
-                         if(count($lexemes)>2){
-                             exit(23);
-                         }
-
                      break;
 
+                 ///<INSTRUCTION> <LABEL> <SYMBOL> <SYMBOL>
                  case 'JUMPIFEQ':
                  case 'JUMPIFNEQ':
-                 echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
+                     if(count($lexemes)!=4){
+                         exit(23);
+                     }
+                     echo(" <instruction order=\"".$in_count."\" opcode=\"".strtoupper($lexemes[0])."\">\n");
                      if(check_label($lexemes[1])){
                          echo("  <arg1 type=\"label\">".$lexemes[1]."</arg1>\n");
                      }
@@ -272,43 +273,19 @@
                          exit(23);
                      }
 
-                     if(count($lexemes)>4){
-                         exit(23);
-                     }
-
                  echo(" </instruction>\n");
                      break;
+                     ///rest after comment, no problem, just adjust the instruction counter
                  case '#':
                      $in_count --;
                     break;
-
+                 ///unknown instruction
                  default:
                      exit(22);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
              }
 
-
-
-
         }
-
-        echo("</program>\n");
-
-    
+        echo("</program>\n"); //end of program
 
 ?>
--- parse_lib.php
+++ parse_lib.php
@@ -1,5 +1,16 @@
 <?php
+    /**
+     * Project: IPP 2023 Parser
+     * @file parse_lib.php
+     * @brief Library for syntactic and lexical analysis of IPPcode23
+     * @author Daniel Žárský <xzarsk04@stud.fit.vutbr.cz>
+     */
 
+    /**
+     * Function checks whether given string is valid variable
+     * @param String loaded from input
+     * @return true in case of valid variable, false otherwise
+     */
    function check_variable($string){
        if(preg_match("/(LF|GF|TF)@[-a-zA-Z_$&%*!?][-a-zA-Z_$&%*!?0-9]*/", trim($string)))//identifier check
        {
@@ -10,11 +21,17 @@
        }
    }
 
+    /**
+     * Function checks whether given string is valid symbol
+     * @param String loaded from input
+     * @return true in case of valid symbol, false otherwise
+     */
    function check_symbol($string){
      if(preg_match("/bool@(true|false)/", $string)){
          return true;
      }
-     else if(preg_match("~^int@[+-]?[0-9]+$~", $string)){ //else if(preg_match("/int@(-|)[0-9]*/", $string)){
+     else if(preg_match("~^int@[+-]?[1-9][0-9]+$~", $string)||preg_match("~^int@[+]?(0[xX][0-9a-fA-F_]+|0[0-7_o]*)$~", $string)||preg_match("~^int@[+-]?[1-9][0-9_]+$~", $string)){ //0xF1_0F
+
          return true;
      }
      else if($string == "nil@nil"){
@@ -31,6 +48,11 @@
 
    }
 
+    /**
+     * Function checks whether given string is valid label
+     * @param String loaded from input
+     * @return true in case of valid label, false otherwise
+     */
    function check_label($string){
 
        if(preg_match("~^[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$~", trim($string)))//identifier check
@@ -42,6 +64,10 @@
        }
    }
 
+    /**
+    * Function process loaded text line by line, checks whether line contains comment, if it does so, it gets rid of the comment
+    * @param One line from input (not processed yet)
+    */
    function cut_comment($line){
         global $comm_count;
         if(strpos($line, "#", 0)){
@@ -54,6 +80,10 @@
         return trim($line);
    }
 
+    /**
+    * Function transform loaded string into correct format xml representation
+    * @param One line from input (not processed yet)
+    */
    function convert_string($input){
 
        $new = htmlspecialchars($input,ENT_QUOTES, null);
